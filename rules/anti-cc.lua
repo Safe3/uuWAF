b3907f2f28213f01bdaf8f02b3ffcf18510b4373
有安科技 <Safe3@users.noreply.github.com>
2022-09-23 22:07:33 +0800

    Update anti-cc.lua

--- rules/anti-cc.lua
+++ rules/anti-cc.lua
@@ -17,13 +17,13 @@ local sh = ngx.shared.ipCache
 local ccIp = 'cc-' .. waf.ip
 local c, f = sh:get(ccIp)
 if not c then
-    sh:set(ccIp, 0, 60, 1)  -- 设置1分钟也就是60秒访问计数时间
+    sh:set(ccIp, 1, 60, 1)  -- 设置1分钟也就是60秒访问计数时间
 else
     if f == 2 then
-        return true, ccIp, true
+        return waf.block(true)
     end
     sh:incr(ccIp, 1)
-    if c >= 360 then
+    if c + 1 >= 360 then
         sh:set(ccIp, c, 300, 2)  -- 设置5分钟也就是300秒拦截时间
     end
 end
