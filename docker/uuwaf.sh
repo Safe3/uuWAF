17dba2d6608a7aed69fe178b1ee60eae04bb22bb
有安科技 <Safe3@users.noreply.github.com>
2024-11-02 10:42:16 +0800

    v6.0.0

--- docker/uuwaf.sh
+++ docker/uuwaf.sh
@@ -16,7 +16,7 @@ fi
 if [ ! $(command -v docker) ]; then
 	curl -sSL https://get.docker.com/ | sh
 	if [ $? -ne "0" ]; then
-		abort "自动安装Docker Engine失败，请参考https://help.aliyun.com/zh/ecs/use-cases/install-and-use-docker-on-a-linux-ecs-instance手工安装后再执行本脚本"
+		abort "自动安装Docker Engine失败，请参考 https://help.aliyun.com/zh/ecs/use-cases/install-and-use-docker-on-a-linux-ecs-instance 手工安装后再执行本脚本"
 	fi
 	systemctl start docker && systemctl enable docker
 fi
